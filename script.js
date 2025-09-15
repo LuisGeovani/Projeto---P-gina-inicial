6f7b2b6bd69df015bae4a6f17f4f8c29858d7bcf
l-geovani <luis.geovani@sou.ufac.br>
2025-09-15 17:22:16 -0500

    correção do carrossel

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
+document.addEventListener('DOMContentLoaded', () => {
+    const track = document.querySelector('.carousel-track');
+    const images = document.querySelectorAll('.carousel-track img');
+    
+    const texts = document.querySelectorAll('.carousel-text');
+    let currentIndex = 0;
+    const intervalTime = 3000;
 
-const swiper = new Swiper (".swiper", {
-    allowSlideNext: true,
-    allowSlidePrev: true,
-    autoplay: false,
-    effect: 'slide',
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+
+    function updateText() {
+        texts.forEach(text => {
+            text.classList.remove('active');
+        });
+        texts[currentIndex].classList.add('active');
+    }
+
+    function moveCarousel() {
+        currentIndex++;
+
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        
+        const imageWidth = images[0].clientWidth;
+        track.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+        
+        updateText();
+    }
+    setInterval(moveCarousel, intervalTime);
+    updateText(); 
 });
